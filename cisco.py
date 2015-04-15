8d26df40d2bcc8355324810be9a65b72bfb0d9eb
R4v3nH0lm <R4v3nH0lm@users.noreply.github.com>
2015-04-15 09:37:36 -0700

    Create cisco.py
    Python script that can be used to collect output from cisco devices. Requires the Exscript python module to be installed. Currently the script is setup to read from a hostfile in the same dir.

---
+++ cisco.py
@@ -0,0 +1,26 @@
+from Exscript.protocols import SSH2
+from Exscript import Account
+
+account = Account('username', 'password')  
+
+hosts_file = open('hostfile', 'r+')
+for ipaddress in hosts_file:
+   
+	conn = SSH2()                       
+	conn.connect(ipaddress)
+	conn.login(account)
+
+	conn.execute('terminal length 0')           
+
+	conn.execute('show run ')
+	print conn.response
+
+	running_configuration = conn.response
+	write_file = open(ipaddress,'w')
+	write_file.write(running_configuration)
+	write_file.close()
+
+	conn.send('exit\r')               
+	conn.close()
+
+hosts_file.close() 
