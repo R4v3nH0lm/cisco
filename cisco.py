11416ce346f5c1a64247affb218eec21cf420a31
R4v3nH0lm <R4v3nH0lm@users.noreply.github.com>
2015-04-15 11:46:55 -0700

    Update cisco.py
    Made it so the script prompts for the command to run and adds that variable to the file name.

--- cisco.py
+++ cisco.py
@@ -1,22 +1,23 @@
 from Exscript.protocols import SSH2
 from Exscript import Account
 
+command = raw_input('Command? ')
 account = Account('username', 'password')  
 
 hosts_file = open('hostfile', 'r+')
 for ipaddress in hosts_file:
-    if ipaddress[0] not "#":
+    if ipaddress[0] is not "#":
         conn = SSH2()                       
         conn.connect(ipaddress)
         conn.login(account)
 
         conn.execute('terminal length 0')           
 
-        conn.execute('show run ')
+        conn.execute(command)
         print conn.response
 
         running_configuration = conn.response
-        write_file = open(ipaddress,'w')
+        write_file = open(ipaddress+' '+command,'w')
         write_file.write(running_configuration)
         write_file.close()
 
