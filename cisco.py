97b25c0358696d67c4428d6f1339ea27f27c5bfa
R4v3nH0lm <R4v3nH0lm@users.noreply.github.com>
2015-05-14 16:04:25 -0700

    Update cisco.py
    Added on more functionality:
    -Translate hostnames from a local hostfile
    -Write output to a subdir
    -Subdir will get a time stamp

--- cisco.py
+++ cisco.py
@@ -1,12 +1,20 @@
+#Import libs
 from Exscript.protocols import SSH2
 from Exscript import Account
+import socket
+import os
+import datetime
 
+#User info
 command = raw_input('Command? ')
 account = Account('username', 'password')  
+date = str(datetime.date.today())
 
+#Translate hostfile and connect to IP
 hosts_file = open('hostfile', 'r+')
-for ipaddress in hosts_file:
-    if ipaddress[0] is not "#":
+for host in hosts_file:
+    if '#' not in host:
+        ipaddress = socket.gethostbyname(host.strip())
         conn = SSH2()                       
         conn.connect(ipaddress)
         conn.login(account)
@@ -16,11 +24,16 @@ for ipaddress in hosts_file:
         conn.execute(command)
         print conn.response
 
+        #Saving output
+        path = command + ' ' + date + '/'
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+        os.chdir(path)
         running_configuration = conn.response
-        write_file = open(ipaddress+' '+command,'w')
+        write_file = open(host+'_'+command,'w')
         write_file.write(running_configuration)
         write_file.close()
-
+        os.chdir('..')
         conn.send('exit\r')               
         conn.close()
 
